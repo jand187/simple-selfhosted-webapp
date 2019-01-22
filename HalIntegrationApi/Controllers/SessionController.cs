9df4e9c8df9da6bc3746caea9c6b2b01dc03cb73
Jan Daniel Andersen (JAXX) <jaxx@oticon.com>
2019-01-22 12:58:33 +0100

    service and website.

---
+++ HalIntegrationApi/Controllers/SessionController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalIntegrationApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] {"value1", "value2"};
+        }
+
+        // POST: api/Default
+        [HttpPost]
+        // public IActionResult Post([FromBody] string sessionId)
+        public IActionResult Post([FromBody] SessionRequest sessionRequest)
+        {
+            return new ContentResult
+            {
+                ContentType = "application/json",
+                Content = sessionRequest.SessionId
+            };
+        }
+    }
+
+    public class SessionRequest
+    {
+        public string SessionId { get; set; }
+    }
+}
